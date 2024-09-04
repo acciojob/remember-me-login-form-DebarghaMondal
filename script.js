8cd65d094815797ae31910d8160bd2fefbe3d5cc
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-09-04 11:01:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-//your JS code here. If required.
+// Function to handle from submission
+
+document.getElementById('loginForm').addEventLIstener('submit', function(e) {
+	e.preventDefault();
+	const username = document.getElementById('username').value;
+	const password = document.getElementById('password').value;
+	const rememberMe = document.getElementById('checkbox').checked;
+
+	if(rememberMe) {
+		localStorage.setItem('username', username);
+		localStorage.setItem('password', password);
+	} else {
+		localStorage.removeItem('username');
+		localStorage.removeItem('password');
+	}
+
+	alert(`Logged in as ${username}`);
+
+	
+// show existing  user button if details are sorted
+	if (localStorage.getItem('username') && localStorage.getItem('password')){
+		document.getElementById('existing').style.display = 'block';
+	}
+});
+
+//Function to handle existing user login
+
+document.getElementById('existing').addEventListener('click', function() {
+	const username = localStorage.getItem('username');
+	alert(`Logged in as ${username}`)
+});
+
+window.onload = function() {
+	if (localStorage.getItem('username') && localStorage.getItem('password')) {
+		document.getElementById('existing').style.display = 'block';
+	}
+}
+
+
+
+
+
+
+
